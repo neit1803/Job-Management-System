659ffe5b895e2f2a2b9d14d62ceb81a1b6a4e7b5
neit1803 <106059540+neit1803@users.noreply.github.com>
2025-04-23 15:03:42 +0700

    Update UserRepository.java

--- user-service/src/main/java/com/tienhuynh/user_service/repository/UserRepository.java
+++ user-service/src/main/java/com/tienhuynh/user_service/repository/UserRepository.java
@@ -2,8 +2,6 @@ package com.tienhuynh.user_service.repository;
 
 import com.tienhuynh.user_service.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
