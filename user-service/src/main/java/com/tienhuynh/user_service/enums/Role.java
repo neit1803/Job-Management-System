57e0c8756555e04101df34d05f078872a393ab7d
neit1803 <106059540+neit1803@users.noreply.github.com>
2025-04-27 12:01:37 +0700

    Update Role.java

--- user-service/src/main/java/com/tienhuynh/user_service/enums/Role.java
+++ user-service/src/main/java/com/tienhuynh/user_service/enums/Role.java
@@ -1,8 +1,8 @@
 package com.tienhuynh.user_service.enums;
 
 public enum Role {
-    ROLE_CANDIDATE,
-    ROLE_RECRUITER,
-    ROLE_ADMIN,
-    ROLE_SYSTEM
+    CANDIDATE,
+    RECRUITER,
+    ADMIN,
+    SYSTEM
 }
