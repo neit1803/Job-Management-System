951f2c5f119112c2e97b63a32c65700ac6d870c9
neit1803 <106059540+neit1803@users.noreply.github.com>
2025-05-12 11:15:54 +0700

    Create JwtService.java

---
+++ auth-service/src/main/java/com/tienhuynh/auth_service/service/JwtService.java
@@ -0,0 +1,10 @@
+package com.tienhuynh.auth_service.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+    public String extractUserName(String jwt) {
+        return null;
+    }
+}
