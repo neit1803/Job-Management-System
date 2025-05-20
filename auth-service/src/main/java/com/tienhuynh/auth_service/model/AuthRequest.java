9467eb6e2acc2cef5448a1b4608c52cfa536bd21
neit1803 <106059540+neit1803@users.noreply.github.com>
2025-05-20 15:35:00 +0700

    Update AuthRequest.java

--- auth-service/src/main/java/com/tienhuynh/auth_service/model/AuthRequest.java
+++ auth-service/src/main/java/com/tienhuynh/auth_service/model/AuthRequest.java
@@ -1,6 +1,13 @@
 package com.tienhuynh.auth_service.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AuthRequest {
     public String email;
-     public String password;
+    public String password;
 }
