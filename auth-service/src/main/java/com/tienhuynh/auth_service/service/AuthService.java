600785b6c393e587e78b957ba0b9d2779102d610
neit1803 <106059540+neit1803@users.noreply.github.com>
2025-05-20 15:35:38 +0700

    Update AuthService.java

--- auth-service/src/main/java/com/tienhuynh/auth_service/service/AuthService.java
+++ auth-service/src/main/java/com/tienhuynh/auth_service/service/AuthService.java
@@ -1,27 +1,46 @@
 package com.tienhuynh.auth_service.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tienhuynh.auth_service.model.AuthRequest;
+import com.tienhuynh.auth_service.model.RegisterRequest;
+import com.tienhuynh.auth_service.model.UserDTO;
+import com.tienhuynh.auth_service.rabbitmq.RabbitMQProducer;
+import com.tienhuynh.auth_service.security.JwtUtil;
+import lombok.RequiredArgsConstructor;
+import org.apache.catalina.User;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class AuthService {
+    private final JwtUtil jwtUtil;
+
     @Autowired
-    JwtService jwtService;
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private RabbitMQProducer rabbitMQProducer;
+
+    @Autowired
+    private ObjectMapper jsonObjectMapper;
 
     public String login(AuthRequest req) {
-        return null;
+        String resp = rabbitMQProducer.getUser(req);
+        UserDTO user = jsonObjectMapper.convertValue(resp, UserDTO.class);
+
+        if (user == null || !passwordEncoder.matches(req.getPassword(), user.getEncodedPassword())) {
+            throw new RuntimeException("Invalid credentials");
+        }
+
+        String accessToken = jwtUtil.generateAccessToken(user.getMail(), user.getRole());
+        String refreshToken = jwtUtil.generateRefreshToken(user.getMail(), user.getRole());
+        return accessToken;
     }
 
-    public String register(AuthRequest req) {
-        return null;
-    }
-
-    public String refreshToken(String token) {
-        return null;
-    }
-
-    public String logout(String token) {
-        return null;
+    public String register(RegisterRequest req) {
+        req.pwd = passwordEncoder.encode(req.pwd);
+        return rabbitMQProducer.saveUser(req);
     }
 }
