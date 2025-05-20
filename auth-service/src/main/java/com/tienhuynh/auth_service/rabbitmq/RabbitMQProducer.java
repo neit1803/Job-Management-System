c0edcf39671d390b75af6efbc0f69b595adb7687
neit1803 <106059540+neit1803@users.noreply.github.com>
2025-05-20 15:35:35 +0700

    Update RabbitMQProducer.java

--- auth-service/src/main/java/com/tienhuynh/auth_service/rabbitmq/RabbitMQProducer.java
+++ auth-service/src/main/java/com/tienhuynh/auth_service/rabbitmq/RabbitMQProducer.java
@@ -1,22 +1,55 @@
 package com.tienhuynh.auth_service.rabbitmq;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.tienhuynh.auth_service.model.AuthRequest;
 import com.tienhuynh.auth_service.model.RegisterRequest;
+import com.tienhuynh.auth_service.model.UserDTO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
-@Service
+@Component
 @Slf4j
 public class RabbitMQProducer   {
-    @Autowired
-    private RabbitTemplate rabbitTemplate;
+    private final RabbitTemplate rabbitTemplate;
 
-    public void sendCheckUserRequest(RegisterRequest req) {
-        rabbitTemplate.convertAndSend("user.exchange", "user.check.request", req);
+    @Autowired
+    private ObjectMapper jsonObjectMapper;
+
+    @Autowired
+    public RabbitMQProducer(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
     }
 
-    public void sendCreateUserRequest(RegisterRequest req) {
-        rabbitTemplate.convertAndSend("user.exchange", "user.save.request", req);
+    public String saveUser(RegisterRequest req) {
+        try {
+            String json = jsonObjectMapper.writeValueAsString(req);
+            return (String) rabbitTemplate.convertSendAndReceive(
+                    RabbitMQConfig.USER_EXCHANGE,
+                    RabbitMQConfig.USER_SAVE_ROUTING_KEY,
+                    json
+            );
+        } catch (JsonProcessingException e) {
+            return e.getMessage();
+        }
+    }
+
+
+    public String getUser(AuthRequest req) {
+
+        try {
+            String json = jsonObjectMapper.writeValueAsString(req);
+            return (String) rabbitTemplate.convertSendAndReceive(
+                    RabbitMQConfig.USER_EXCHANGE,
+                    RabbitMQConfig.USER_GET_ROUTING_KEY,
+                    json
+            );
+        } catch (JsonProcessingException e) {
+            log.error("Error serializing AuthRequest object", e);
+            return e.getMessage();
+        }
+
     }
 }
