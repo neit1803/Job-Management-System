a913f223e4ea5b3075f19c461ecacc6dc1a9082b
neit1803 <106059540+neit1803@users.noreply.github.com>
2025-05-20 15:36:06 +0700

    Create SecurityConfig.java

---
+++ auth-service/src/main/java/com/tienhuynh/auth_service/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.tienhuynh.auth_service.config;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/auth/**").permitAll()
+                        .anyRequest().authenticated()
+                );
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
