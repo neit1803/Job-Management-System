3ebfa93d05e3b81b0c67e5dcf5296fef9ae0a358
neit1803 <106059540+neit1803@users.noreply.github.com>
2025-05-20 15:34:45 +0700

    Update AuthController.java

--- auth-service/src/main/java/com/tienhuynh/auth_service/controller/AuthController.java
+++ auth-service/src/main/java/com/tienhuynh/auth_service/controller/AuthController.java
@@ -1,10 +1,9 @@
 package com.tienhuynh.auth_service.controller;
 
 import com.tienhuynh.auth_service.model.AuthRequest;
-import com.tienhuynh.auth_service.model.AuthResponse;
 import com.tienhuynh.auth_service.model.RegisterRequest;
-import com.tienhuynh.auth_service.rabbitmq.RabbitMQProducer;
 import com.tienhuynh.auth_service.service.AuthService;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -15,19 +14,16 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 public class AuthController {
     @Autowired
-    AuthService authService;
-
-    @Autowired
-    private RabbitMQProducer producer;
+    private AuthService authService;
 
     @PostMapping("/login")
-    public ResponseEntity<AuthResponse> login(@RequestBody AuthRequest req) {
-        return ResponseEntity.ok(new AuthResponse());
+    public ResponseEntity<String> login(@RequestBody AuthRequest req) {
+        return ResponseEntity.ok(authService.login(req));
     }
 
     @PostMapping("/register")
-    public ResponseEntity<String> register(@RequestBody RegisterRequest req) {
-        producer.sendCheckUserRequest(req);
-        return ResponseEntity.ok("Hello");
+    public ResponseEntity<String> register(@Valid @RequestBody RegisterRequest req) {
+        log.info("Register request: {}", req);
+        return ResponseEntity.ok(authService.register(req));
     }
 }
