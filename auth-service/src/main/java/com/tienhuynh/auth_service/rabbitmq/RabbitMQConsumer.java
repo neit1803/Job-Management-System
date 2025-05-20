88eb17aa5079404dfca196ee49ceedd54bcd2abc
neit1803 <106059540+neit1803@users.noreply.github.com>
2025-05-20 15:35:32 +0700

    Update RabbitMQConsumer.java

--- auth-service/src/main/java/com/tienhuynh/auth_service/rabbitmq/RabbitMQConsumer.java
+++ auth-service/src/main/java/com/tienhuynh/auth_service/rabbitmq/RabbitMQConsumer.java
@@ -7,27 +7,27 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class RabbitMQConsumer {
-    @Autowired
-    private RabbitMQProducer authProducer;
-
-    private PasswordEncoder passwordEncoder;
-
-    public RabbitMQConsumer(RabbitMQProducer authProducer, PasswordEncoder passwordEncoder) {
-        this.authProducer = authProducer;
-        this.passwordEncoder = passwordEncoder;
-    }
-
-    @RabbitListener(queues = "auth.check.response.queue")
-    public void handleCheckUserResponse(UserCheckResponse response) {
-        if (!response.exists()) {
-            // Hash password
-            UserRegisterDTO dto = response.getOriginalDTO();
-            dto.setPassword(passwordEncoder.encode(dto.getPassword()));
-
-            // Gửi yêu cầu lưu user
-            authProducer.sendCreateUserRequest(dto);
-        } else {
-            // Username/email đã tồn tại → xử lý lỗi
-        }
-    }
+//    @Autowired
+//    private RabbitMQProducer authProducer;
+//
+//    private PasswordEncoder passwordEncoder;
+//
+//    public RabbitMQConsumer(RabbitMQProducer authProducer, PasswordEncoder passwordEncoder) {
+//        this.authProducer = authProducer;
+//        this.passwordEncoder = passwordEncoder;
+//    }
+//
+//    @RabbitListener(queues = "auth.check.response.queue")
+//    public void handleCheckUserResponse(UserCheckResponse response) {
+//        if (!response.exists()) {
+//            // Hash password
+//            UserRegisterDTO dto = response.getOriginalDTO();
+//            dto.setPassword(passwordEncoder.encode(dto.getPassword()));
+//
+//            // Gửi yêu cầu lưu user
+//            authProducer.sendCreateUserRequest(dto);
+//        } else {
+//            // Username/email đã tồn tại → xử lý lỗi
+//        }
+//    }
 }
