fd62de2de7137d8d300dfe4e2647312cd007a99e
neit1803 <106059540+neit1803@users.noreply.github.com>
2025-05-20 15:34:55 +0700

    Update UserDTO.java

--- auth-service/src/main/java/com/tienhuynh/auth_service/model/UserDTO.java
+++ auth-service/src/main/java/com/tienhuynh/auth_service/model/UserDTO.java
@@ -4,12 +4,16 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class UserDTO {
+public class UserDTO implements Serializable {
     private String id;
-    private String email;
+    private String mail;
     private String encodedPassword;
+    private String fullName;
+    private String phone;
     private String role;
 }
